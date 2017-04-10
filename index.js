b340c61dcd51deb76b202aaecf73c9d6268adb4d
jhamet93 <jhamet93@gmail.com>
2017-04-10 17:56:05 -0400

    Added config to loader

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var dot = require('dot');
 var fs = require('fs');
+var loader-utils = require('loader-utils');
+var path = require('path');
+
+function findConfig(dir, filename){
+	var path = path.join(dir, filename);
+	if (fs.existsSync(path)){
+		return fs.readFileSync(path);
+	}
+
+	var parentDir = path.dirname(dir);
+	if (parentDir !== dir){
+		return findConfig(parentDir, filename);
+	}
+}
 
 module.exports = function(content) {
   if (this.cacheable) {
     this.cacheable();
   }
-  
+
+  const templateSettings = findConfig(path.dirname(this.context), '.dotrc');
+  dot.templateSettings = templateSettings;
   dot.templateSettings.selfcontained = true;
 
   var content = fs.readFileSync(this.resourcePath);
