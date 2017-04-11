f39132e2067a616e673bc4bf42390249acb0cc72
jhamet93 <jhamet93@gmail.com>
2017-04-11 09:40:12 -0400

    Merge configuration into default configuration for dot

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ var path = require('path');
 function findConfig(dir, filename){
 	var configPath = path.join(dir, filename);
 	if (fs.existsSync(configPath)){
-		return fs.readFileSync(configPath);
+		return JSON.parse(fs.readFileSync(configPath, {
+			encoding: 'utf-8'
+		}));
 	}
 
 	var parentDir = path.dirname(dir);
 	if (parentDir !== dir){
 		return findConfig(parentDir, filename);
 	}
+
+	return {};
 }
 
 module.exports = function(content) {
@@ -19,10 +23,10 @@ module.exports = function(content) {
     this.cacheable();
   }
 
-  const templateSettings = findConfig(path.dirname(this.context), '.dotrc');
-  dot.templateSettings = templateSettings;
   dot.templateSettings.selfcontained = true;
-
+  var templateSettings = findConfig(path.dirname(this.context), '.dotrc');
+  dot.templateSettings = Object.assign(dot.templateSettings, templateSettings);
+  
   var content = fs.readFileSync(this.resourcePath);
   return "module.exports = " + dot.template(content);
 };
